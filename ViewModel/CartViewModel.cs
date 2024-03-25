22c0aed39ee5953cd7fa7d36051f89c53e1fc2f1
Toan Tran Cong <22110248@student.hcmute.edu.vn>
2024-03-25 14:29:07 +0700

    Update: All

--- ViewModel/CartViewModel.cs
+++ ViewModel/CartViewModel.cs
@@ -2,8 +2,10 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace Exchange_App.ViewModel
 {
@@ -11,19 +13,31 @@ namespace Exchange_App.ViewModel
     {
         private User _currentUser;
 
+        private List<object> _cartItems;
+
         public CartViewModel(User user)
         {
             CurrentUser = user;
+
+            var custQuery = from cart in CurrentUser.CartItems
+                            join product in DataProvider.Ins.DB.Products
+                            on cart.ProductID equals product.ProductID
+                            select new {
+                                ProductID = product.ProductID,
+                                Original_price = product.Original_price,
+                                Sell_price = product.Sell_price,
+                                ProductName = product.ProductName,
+                                PreviewImage = product.GetPreviewImage,
+                                Category = product.Category,
+                                Product_Quantity = product.Quantity,
+                                CartItemsID = cart.CartItemsID,
+                                CartQuantity = cart.Quantity
+                            };
+                    MessageBox.Show("Cart Items: " + custQuery.Count().ToString() + " items in cart.");
+            CartItems = custQuery.ToList<object>();
         }
 
         public User CurrentUser { get => _currentUser; set => _currentUser=value; }
-
-        public ICollection<CartItem> cartItems { get { 
-            
-                // get list of product from CartItems
-                
-
-            } set {; } }
-
+        public List<object> CartItems { get => _cartItems; set => _cartItems=value; }
     }
 }
